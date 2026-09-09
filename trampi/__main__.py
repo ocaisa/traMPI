0516b3cff35a06d1de1f01b977836ebeafb255eb
Alan O'Cais <alan.ocais@cecam.org>
2026-09-09 21:34:07 +0200

    What is going on with this patch!

--- trampi/__main__.py
+++ trampi/__main__.py
@@ -63,24 +63,22 @@ def apply_header_patch(header, header_patch, output_header):
     output_header = Path(output_header)
     output_header.parent.mkdir(parents=True, exist_ok=True)
 
-    # Ensure the original header is writable.
-    orig_path = Path(header)
-    mode = orig_path.stat().st_mode
-    os.chmod(orig_path, mode | stat.S_IWUSR)
+    # Copy the original header.
+    shutil.copy2(header, output_header)
 
-    # Apply the patch in-place to the original header.
+    # Ensure the copied file is writable.
+    mode = output_header.stat().st_mode
+    os.chmod(output_header, mode | stat.S_IWUSR)
+
+    # Apply the patch in-place.
     subprocess.run(
         [
             "patch",
-            "-l",
-            str(orig_path),
+            str(output_header),
             str(header_patch),
         ],
         check=True,
     )
-
-    # Copy the patched header to the destination.
-    shutil.copy2(orig_path, output_header)
 
 
 def main():
